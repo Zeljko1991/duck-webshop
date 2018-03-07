c44ed4f0b6f553fcf52fb365e3164ac63c148cd4
NoCrazyShet <j.pettit51@gmail.com>
2018-03-07 10:27:23 +0100

    phpReq

---
+++ bigDucks.php
@@ -0,0 +1,37 @@
+<?php require 'head.php'; ?>
+<body>
+<!-- Header section -->
+<header> <?php require 'nav.php'; ?> </header>
+
+<!-- Main section -->
+<main>
+    <ul class="collection">
+        <li class="collection-item avatar">
+            <img src="images/yuna.jpg" alt="" class="circle">
+            <span class="title">Title</span>
+            <p>First Line <br>Second Line</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+        <li class="collection-item avatar">
+            <i class="material-icons circle">folder</i>
+            <span class="title">Big Yellow Duck</span>
+            <p>Top quality duck made from the best rubber known to man <br>Something more?</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+        <li class="collection-item avatar">
+            <i class="material-icons circle green">insert_chart</i>
+            <span class="title">Title</span>
+            <p>First Line <br>Second Line</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+        <li class="collection-item avatar">
+            <i class="material-icons circle red">play_arrow</i>
+            <span class="title">Title</span>
+            <p>First Line <br>Second Line</p>
+            <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+        </li>
+    </ul>
+</main>
+
+<!-- Footer -->
+<?php require 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<?php
+?>
+<!-- Footer -->
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            © 2018 Copyright @ ***
+            <a class="grey-text text-lighten-4 right" href="#">Top Duck</a>
+        </div>
+    </div>
+</footer>
+
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="materialize-src/js/bin/materialize.min.js"></script>
+<script type="text/javascript">
+    $('.button-collapse').sideNav();
+    $('.collapsible').collapsible();
+    $(document).ready(function(){
+        $('.carousel.carousel-slider').carousel({fullWidth: true});
+    });
+</script>
+</body>
+</html>
